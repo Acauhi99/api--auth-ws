9e85bd3fa2511788c46dd61b0c910a3ea4dd9e60
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-23 00:45:26 -0300

    add: conexao com banco

---
+++ src/db/sequelize.ts
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db/database.sqlite",
+  logging: false,
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+  } catch (error) {
+    console.error("Não foi possível conectar ao banco de dados:", error);
+  }
+};
+
+export { connectDB, sequelize };
