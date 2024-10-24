82cb56531d487a7d9dbd09125f4d77c721251bf5
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-23 23:33:55 -0300

    add: schema para o model de usuario

--- src/domain/user/user.model.ts
+++ src/domain/user/user.model.ts
@@ -1,34 +1,52 @@
+import kuid from "kuid";
 import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../../db/sequelize";
 
-class User extends Model {
-  public id!: number;
-  public username!: string;
+export class User extends Model {
+  public id!: string;
+  public firstName!: string;
+  public lastName!: string;
   public email!: string;
+  public password!: string;
+  public birthDate!: Date;
 }
 
 User.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+      type: DataTypes.STRING,
+      defaultValue: kuid,
       primaryKey: true,
     },
-    username: {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    birthDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
     },
   },
   {
     sequelize,
-    modelName: "User",
     tableName: "users",
     timestamps: true,
   }
 );
 
-export default User;
+(async () => {
+  await User.sync();
+})();
