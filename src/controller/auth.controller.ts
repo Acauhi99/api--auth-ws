e98a46a6520b721ea21d174cc88092a29ac8729c
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-23 23:34:33 -0300

    refactor: melhoria dos imports

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { AuthService } from "../domain/auth/auth.service";
+import { AuthService } from "../domain";
 
 export class AuthController {
   private authService: AuthService;
