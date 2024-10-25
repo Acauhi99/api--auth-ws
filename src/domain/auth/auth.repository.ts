ce9ced07ed843bd3740bcf8d3bf313520d8861bf
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-25 10:23:44 -0300

    style: espacamento

--- src/domain/auth/auth.repository.ts
+++ src/domain/auth/auth.repository.ts
@@ -5,6 +5,7 @@ export class AuthRepository {
   async findByEmail(email: string): Promise<User | null> {
     try {
       const user = await User.findOne({ where: { email } });
+
       return user;
     } catch (error) {
       throw new Error("Erro ao buscar usuário");
@@ -14,6 +15,7 @@ export class AuthRepository {
   async save(userData: UserCreateFieldsDTO): Promise<User> {
     try {
       const newUser = await User.create(userData as any);
+
       return newUser;
     } catch (error) {
       throw new Error("Erro ao criar usuário");
