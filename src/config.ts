2ec0cc7ff24f2466758b2bc4e7f86a31a527abeb
Mateus Acauhi <mateusacauhi@bluems.com>
2024-10-26 15:35:09 -0300

    add: variaveis de ambiente do github OAuth

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+export const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI;
