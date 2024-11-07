2ed18ac06a543063ed7abf9fa10877827e4b79e2
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-07 18:00:23 -0300

    add: variavel de ambiente para api externa

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,4 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 export const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI;
+export const API_KEY_STOCKS_DATA = process.env.API_KEY_STOCKS_DATA;
