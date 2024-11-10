c27e2a1ce6b80d5faba15ea2cd5a0e0bd431dfe1
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-10 11:10:16 -0300

    add: export de dtos de wallet

---
+++ src/domain/wallet/dtos/index.ts
@@ -0,0 +1,4 @@
+export { DepositDTO } from "./deposit.dto";
+export { WithdrawDTO } from "./withdraw.dto";
+export { ProfitabilityDTO } from "./profitability.dto";
+export { WalletSummaryDTO } from "./wallet-summary.dto";
