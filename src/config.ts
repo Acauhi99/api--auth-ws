9bd39f9405d00ad639377bc22e6e0163e192e775
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-12 23:22:47 -0300

    refactor: troca de nome de variavel de ambiente

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,6 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 export const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI;
-export const API_KEY_STOCKS_DATA = process.env.API_KEY_STOCKS_DATA;
+export const BRAPI_TOKEN = process.env.BRAPI_TOKEN;
+export const BRAPI_URL = process.env.BRAPI_URL;
 export const DB_ENV = process.env.DB_ENV;
