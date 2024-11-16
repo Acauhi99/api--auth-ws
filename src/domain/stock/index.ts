49c7d40acb66a0ea3dabd530473904694c08eeef
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-16 17:10:22 -0300

    add: dependencias de test

--- src/domain/stock/index.ts
+++ src/domain/stock/index.ts
@@ -1,3 +1,4 @@
 export { Stock } from "./stock.model";
+export { StockType } from "./stock.model";
 export { StockService } from "./stock.service";
 export { StockRepository } from "./stock.repository";
