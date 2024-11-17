ad7988c8686763b54c1244cb2afd48f11d71ebb0
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-17 20:25:41 -0300

    delete: comentarios

--- src/domain/transaction/transaction.route.ts
+++ src/domain/transaction/transaction.route.ts
@@ -7,7 +7,6 @@ const transactionController = new TransactionController();
 
 transactionRouter.use(authenticateHandler);
 
-// Stock operations
 transactionRouter.post(
   "/stock/buy",
   asyncHandler(transactionController.buyStock)
@@ -17,14 +16,12 @@ transactionRouter.post(
   asyncHandler(transactionController.sellStock)
 );
 
-// Money operations
 transactionRouter.post("/deposit", asyncHandler(transactionController.deposit));
 transactionRouter.post(
   "/withdraw",
   asyncHandler(transactionController.withdraw)
 );
 
-// History
 transactionRouter.get(
   "/history",
   asyncHandler(transactionController.getTransactionHistory)
