7e8fb2bd45e8da8079f06f48479385537ff296a3
Mateus Acauhi <mateusacauhi@bluems.com>
2024-11-20 17:56:25 -0300

    delete: retirado auth da rota de listagem de acoes

--- src/domain/stock/stock.route.ts
+++ src/domain/stock/stock.route.ts
@@ -5,8 +5,6 @@ import { asyncHandler, authenticateHandler } from "../../middlewares";
 const stockRouter = Router();
 const stockController = new StockController();
 
-stockRouter.use(authenticateHandler);
-
 stockRouter.get(
   "/market/available",
   asyncHandler(stockController.getAvailableStocks)
